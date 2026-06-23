06b3ce0a71fd809cdaf158b89ab43a1f0acd3b8f
Darwin Billiano <darwinbillian@outlook.com>
2026-06-23 19:11:26 +0700

    feat(home): add hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Portfolio",
 };
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${spaceGrotesk.variable} font-sans bg-neutral-950 text-neutral-300`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { LucideMail } from "lucide-react";
+
 export default function Home() {
   return (
-    <main className="p-16">
-      <h1 className="text-3xl">Portfolio</h1>
+    <main>
+      <Hero />
     </main>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center px-16">
+      <section className="text-center">
+        <p className="text-2xl font-bold text-neutral-100">
+          Hi! I'm <span className="text-red-400">Darwin Billiano</span>
+        </p>
+        <h1 className="mt-4 text-6xl font-bold text-neutral-100">
+          Software Developer
+        </h1>
+        <p className="mt-4">
+          Based in Indonesia &middot; Open-Source Contributor
+        </p>
+      </section>
+    </div>
   );
 }
