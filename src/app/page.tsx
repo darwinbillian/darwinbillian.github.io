eb92500e035041be8c8b8e042fca54dcf708c253
Darwin Billiano <darwinbillian@outlook.com>
2026-06-23 19:12:12 +0700

    feat(home): add contacts into hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,21 @@ function Hero() {
         <p className="mt-4">
           Based in Indonesia &middot; Open-Source Contributor
         </p>
+        <div className="mt-6 flex justify-center gap-4">
+          <a
+            href="https://github.com/darwinbillian"
+            target="_blank"
+            className="transition hover:text-neutral-100"
+          >
+            <SiGithub />
+          </a>
+          <a
+            href="mailto:darwinbillian@outlook.com"
+            className="transition hover:text-neutral-100"
+          >
+            <LucideMail />
+          </a>
+        </div>
       </section>
     </div>
   );
